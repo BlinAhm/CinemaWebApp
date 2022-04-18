3e225a828b4cde29fdd3a18cfd19ed1229b4feec
Blin Ahmeti <ba51658@ubt-uni.net>
2022-04-18 22:51:37 +0200

    more_logos

--- cinema-web-app/src/components/UI/Footer/FooterSocials.js
+++ cinema-web-app/src/components/UI/Footer/FooterSocials.js
@@ -1,11 +1,11 @@
-import './FooterSocials.css'; 
+import './FooterSocials.css';
 
 const FooterSocials = () => {
     return (
         <div className="footer-socials">
             <h4>FOLLOW US</h4>
-            <a href="https://Facebook.com"><img src="" alt="facebook"/></a>
-            <a href="https://Twitter.com"><img src="" alt="twitter"/></a>
+            <a href="https://Facebook.com"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/1200px-Facebook_Logo_%282019%29.png" alt="facebook" /></a>
+            <a href="https://Twitter.com"><img src="https://cdn4.iconfinder.com/data/icons/social-media-icons-the-circle-set/48/twitter_circle-512.png" alt="twitter" /></a>
         </div>
     );
 }
