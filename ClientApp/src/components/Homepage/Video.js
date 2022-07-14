125bade22c8fcd4c5c411891ee7124c0a817ae2a
aarbermorina <am51551@ubt-uni.net>
2022-07-14 16:01:41 +0200

    add video

---
+++ ClientApp/src/components/Homepage/Video.js
@@ -0,0 +1,17 @@
+﻿import { useState } from 'react';
+import ReactPlayer from "react";
+import './Video.css';
+import React from "react";
+import bgVideo from '../../video/video-highlight.mp4';
+
+function Video(){
+    return (
+        <div classname="background">
+            <video autoPlay loop muted plays-inline>
+                <source src={bgVideo} type="video/mp4" />
+            </video>
+        </div>
+    );
+}
+export default Video;
+
--- ClientApp/src/pages/Homepage.js
+++ ClientApp/src/pages/Homepage.js
@@ -1,8 +1,10 @@
+import Video from "../../src/components/Homepage/Video"
 import Faq from "../../src/components/Homepage/Faq"
 const Homepage = () => {
     
     return (
         <div className="main">
+            <Video />
             <Faq />
         </div>
     );
